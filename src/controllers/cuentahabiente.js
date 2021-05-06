f466807255dded2873e8c2dbadf95b8423991c69
Cristian Castellanos <cristian6cas@hotmail.es>
2021-05-06 02:21:27 -0600

    Se termino el reporte 3 (entre comillas) y reporte 5

--- src/controllers/cuentahabiente.js
+++ src/controllers/cuentahabiente.js
@@ -1,16 +1,54 @@
-const { getTransactions } = require("../db/cuentahabiente")
+const { getTransactions, getTransactionsByMonth } = require("../db/cuentahabiente")
 
 async function indexCthabiente(req, res){
-  /*Pueden ser todas o por mes*/
-  //res.send('Get operaciones por cuenta habiente [por mes]');
-  res.render('cuentahabiente',{transacciones:[]})
+  res.render('cuentahabiente',{
+      ctahabiente:{
+        name: '',
+        lastname: '',
+        cui: ''
+      },
+      transacciones:[]
+    })
 }
 
 async function getOperacionesCthabiente(req, res){
     console.log(`REQUEST:: ${JSON.stringify(req.body,null,2)}`)
-    const r = await getTransactions(req.body.cui);
-    console.log(r.rows)
-    res.render('cuentahabiente',{transacciones:r.rows})
+    let transacciones = [];
+    if(req.body.mes == ''){
+      const r = await getTransactions(req.body.cui);
+      transacciones = r.rows;
+    } else {
+      const month_year = req.body.mes.split('/')
+      const formattedDate = month_year[1] + '-' + month_year[0] + '-'
+      const r = await getTransactionsByMonth(req.body.cui,formattedDate + '01', formattedDate + getLastDay(month_year[0]));
+      transacciones = r.rows;
+    }
+    let ctahabiente = {
+      name: transacciones[0].name_cta,
+      lastname: transacciones[0].lastname_cta,
+      cui:req.body.cui
+    }
+    res.render('cuentahabiente',{ctahabiente,transacciones})
+}
+
+function getLastDay(month){
+  switch(parseInt(month)){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 28;
+  }
 }
 
 module.exports = {
--- src/db/cuentahabiente.js
+++ src/db/cuentahabiente.js
@@ -1,11 +1,13 @@
 const db = require('../services/cassandra');
 
 async function getTransactions(cui){
-  return await db.query('SELECT * FROM transactions_by_cuentahabiente WHERE cui = :cui',[cui])
+  return await db.query('SELECT * FROM transactions_by_cuentahabiente WHERE cui = ?',[cui])
 }
 
-async function getTransactionsByMonth(cui,month){
-
+async function getTransactionsByMonth(cui, fecha_ini, fecha_fin){
+  const query = 'SELECT * from transactions_by_cuentahabiente_month WHERE cui = ? and fecha_generacion > \''+fecha_ini+'\' and fecha_generacion < \'' + fecha_fin + '\''
+  return await db.query(query,[cui])
+  ;
 }
 
 module.exports = {
